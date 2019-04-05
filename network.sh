5b981bd847e1d8c907fc7c7ddf771d3ffdee4ce2
Joel Gilley <gilleyj@gmail.com>
2019-04-05 18:28:34 -0400

    Remove bad network

--- network.sh
+++ network.sh
@@ -14,12 +14,6 @@ if [ "$1" == "start" ]; then
       --subnet=172.26.0.0/16 \
       --gateway=172.26.0.1 \
       network_proxy
-    docker network create \
-      --driver=bridge \
-      --attachable \
-      --internal \
-      --subnet=172.28.0.0/16 \
-      network_db
 elif [ "$1" == "stop" ]; then
     echo "Stopping network"
     docker network rm network_frontend
